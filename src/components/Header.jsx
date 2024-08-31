c51602707ad0063f647bf49a895acf5ca07f7c14
Sumitprajapatintv <sumit963605@gmail.com>
2024-08-31 23:04:51 +0530

    changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,15 +6,15 @@ function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Noida</span>
-            <span className="text-slate-700"> Estate</span>
+            <span className="text-slate-500">Urban</span>
+            <span className="text-slate-700">Nest</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form className="bg-slate-200 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-tranparent focus:outline-node w-24 sm:w-24"
+            className="bg-tranparent focus:outline-node w-24 sm:w-64"
           />
           <FaSearch className="text-slate-600" />
         </form>
@@ -22,16 +22,12 @@ function Header() {
         <ul className="flex gap-4">
           <Link to="/">
             {" "}
-            <li className="hidden sm-inline text-slate-700 hover:underline">
-              Home
-            </li>
+            <li className="sm-inline text-slate-700 hover:underline">Home</li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm-inline text-slate-700 hover:underline">
-              About
-            </li>
+            <li className="sm-inline text-slate-700 hover:underline">About</li>
           </Link>
-          <Link to="/signin">
+          <Link to="/sigin-in">
             {" "}
             <li className="sm-inline text-slate-700 hover:underline">SignIn</li>
           </Link>
