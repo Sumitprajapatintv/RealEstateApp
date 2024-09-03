eda838750e4a45f51b6e2d718c90facee4788da3
Sumitprajapatintv <sumit963605@gmail.com>
2024-09-03 22:05:33 +0530

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://prajapatisumit013:yVRTP9tnb0dOJyJ2@cluster0.a7otg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const app = express();
 
