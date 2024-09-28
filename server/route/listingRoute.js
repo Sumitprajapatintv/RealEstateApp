ef7e7ff1fdb212667f7f6bb5a6094d95865c0b84
Sumitprajapatintv <sumit963605@gmail.com>
2024-09-28 22:17:34 +0530

    Show listing Funcationalty

--- server/route/listingRoute.js
+++ server/route/listingRoute.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import { createListing } from './../controller/listingController.js';
+import { createListing,getUserListings } from './../controller/listingController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 router.post('/create', verifyToken, createListing);
+
+router.get('/listings/:id', verifyToken, getUserListings)
 export default router;
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -8,4 +8,6 @@ router.post('/update/:id',verifyToken,updateUser);
 router.delete('/delete/:id',verifyToken,deleteUser);
 
 
+
+
 export default router;
