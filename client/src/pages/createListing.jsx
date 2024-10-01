79a7a833d7d8b2bfbac941d0f9ce64a8e930a977
Sumitprajapatintv <sumit963605@gmail.com>
2024-10-01 22:50:16 +0530

    Contact Functionalty

--- client/src/pages/createListing.jsx
+++ client/src/pages/createListing.jsx
@@ -275,7 +275,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && 
+                  <span className='text-xs'>($ / month)</span>
+                }
               </div>
             </div>
             {
@@ -291,7 +293,9 @@ export default function CreateListing() {
                   value={formData.discountPrice}
                 />
                 <div className='flex flex-col items-center'>
-                  <p>Discounted price</p>
+                {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                   <span className='text-xs'>($ / month)</span>
                 </div>
             </div>
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { updateUser,deleteUser } from '../controller/updateUser.controller.js';
+import { updateUser,deleteUser ,getUser} from '../controller/updateUser.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router=express.Router();
 
@@ -7,7 +7,7 @@ router.post('/update/:id',verifyToken,updateUser);
 
 router.delete('/delete/:id',verifyToken,deleteUser);
 
-
+router.get('/:id', verifyToken, getUser)
 
 
 export default router;
